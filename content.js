38bcbd1f1635e9608d2a9c9ab7e52e8e72490b34
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2023-01-15 16:41:08 +0900

    AtCoderに対応

--- content.js
+++ content.js
@@ -4,12 +4,22 @@
         (e) => {
             if ((e.ctrlKey || e.metaKey) && e.code === 'Enter') {
                 const urlPath = location.pathname;
-                let submitButton = '';
-                if (urlPath.includes('atcoder_problems')) {
+                let submitButton = null;
+                if (urlPath.includes('contests')) {
+                    // AtCoder
+                    console.log('called');
+                    submitButton = document.getElementById('submit');
+                } else if (urlPath.includes('atcoder_problems')) {
+                    // TOPSIC
+                    // AtCoder過去問
                     submitButton = document.getElementById('submit_btn');
                 } else if (urlPath.includes('problem_detail')) {
+                    // TOPSIC
+                    // 受験=>問題文
                     submitButton = document.getElementById('save_btn');
-                } else if (urlPath.includes('resubmit')) {
+                } else {
+                    // TOPSIC
+                    // 受験履歴=>再提出
                     submitButton = document.getElementById(
                         'execute_exam_retake'
                     );
